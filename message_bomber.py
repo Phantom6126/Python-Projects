e7e3501e4ab3f0fdca458cd3c3b3d605db97ccc8
Phantom6126 <kanakesh8888@gmail.com>
2024-12-04 06:44:49 +0530

    Create message_bomber.py

---
+++ message_bomber.py
@@ -0,0 +1,15 @@
+# for pyautogui installation , terminal command : pip install pyautogui
+
+import pyautogui as pg  # install this library to cotrol the cursor 
+import time
+import random
+
+animals = ['monkey', 'donkey', 'dog']
+
+time.sleep(8)  # time limit to place the cursor
+
+for i in range(5):              # range for number of messages
+    a = random.choice(animals)  # chooses a random element from the list
+    pg.write(f"You are a {a}")  # types the message in the text area
+    pg.press('enter')           # presses the enter to send the message
+
